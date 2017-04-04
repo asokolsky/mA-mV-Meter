8ec6963108e514f11b99e142b3e264e7548afbc5
Alex Sokolsky <asokolsky@yahoo.com>
2017-04-04 16:57:30 -0700

    Battery monitor added

--- mA-mV-Meter.ino
+++ mA-mV-Meter.ino
@@ -3,9 +3,11 @@
 #include <U8g2lib.h>
 #include <Adafruit_INA219.h>
 #include "PinButton.h"
+#include "BatteryMonitor.h"
 #include "Trace.h"
 
-Adafruit_INA219 ina219;
+Adafruit_INA219 g_ina219;
+BatteryMonitor g_batteryMonitor(A0);
 
 #if defined(ARDUINO_ARCH_SAMD)
 // for Zero, output on USB Serial console, remove line below if using programming port to program the Zero!
@@ -13,15 +15,15 @@ Adafruit_INA219 ina219;
 #endif
 
 
-//U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
-U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C g_u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+//U8G2_SH1106_128X64_NONAME_F_HW_I2C g_u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
-//u8g2_uint_t g_uDisplayWidth = 0;
-//u8g2_uint_t g_uDisplayHeight = 0;
-//u8g2_uint_t g_uColUnits = 0;
+//g_u8g2_uint_t g_uDisplayWidth = 0;
+//g_u8g2_uint_t g_uDisplayHeight = 0;
+//g_g_u8g2_uint_t g_uColUnits = 0;
 
 /** update the display this often (in ms) */
-int16_t g_iDisplayUpdate = 1;
+int16_t g_iDisplayUpdate = 10;
 
 class MyButton : public PinButton
 {
@@ -41,7 +43,7 @@ public:
   bool onKeyDown()
   {
     DEBUG_PRINTLN("MyButton::onKeyDown()");
-    return false;
+    return true;
   }
   bool onLongKeyDown()
   {
@@ -51,7 +53,7 @@ public:
   bool onKeyUp(bool bLong)
   {
     DEBUG_PRINT("MyButton::onKeyUp(bLong=");DEBUG_PRINTDEC(bLong);DEBUG_PRINTLN(")");  
-    return false;
+    return true;
   }
   bool onClick()
   {
@@ -62,7 +64,7 @@ public:
       DEBUG_PRINT("g_iDisplayUpdate=");DEBUG_PRINTDEC(g_iDisplayUpdate);DEBUG_PRINTLN("");  
       if(i >= (sizeof(iDisplayUpdateOptions)/sizeof(iDisplayUpdateOptions[0])))
         i = 0;
-    return false;
+    return true;
   }
   bool onDoubleClick()
   {
@@ -85,17 +87,17 @@ void setup(void)
   // Initialize the INA219.
   // By default the initialization will use the largest range (32V, 2A).  However
   // you can call a setCalibration function to change this range (see comments).
-  ina219.begin();
+  g_ina219.begin();
   // To use a slightly lower 32V, 1A range (higher precision on amps):
-  //ina219.setCalibration_32V_1A();
+  //g_ina219.setCalibration_32V_1A();
   // Or to use a lower 16V, 400mA range (higher precision on volts and amps):
-  //ina219.setCalibration_16V_400mA();
+  //g_ina219.setCalibration_16V_400mA();
   
    
-  u8g2.begin();
-  //g_uDisplayWidth = u8g2.getWidth();
-  //g_uColUnits = u8g2.getWidth() - 35; 
-  //g_uDisplayHeight = u8g2.getHeight();
+  g_u8g2.begin();
+  //g_uDisplayWidth = g_u8g2.getWidth();
+  //g_uColUnits = g_u8g2.getWidth() - 35; 
+  //g_uDisplayHeight = g_u8g2.getHeight();
 }
 
 /** draw batttery gauge */
@@ -103,67 +105,69 @@ void drawBattery(u8g2_uint_t x, u8g2_uint_t y, uint8_t percentfull)
 {
   u8g2_uint_t w = 14;
   u8g2_uint_t h = 8;
-  u8g2.drawFrame(x, y, w, h);
-  u8g2.drawVLine(x+w, y+3, h-6);
+  g_u8g2.drawFrame(x, y, w, h);
+  g_u8g2.drawVLine(x+w, y+3, h-6);
   x += 2;
   y += 2;
   h -= 4;
   w -= 4;
   if(percentfull > 10)
-    u8g2.drawBox(x, y, (percentfull > 90) ? w : map(percentfull, 0, 100, 0, w), h);
+    g_u8g2.drawBox(x, y, (percentfull > 90) ? w : map(percentfull, 0, 100, 0, w), h);
 }
 
 /** draw title bar including batttery gauge */
 void drawTitleBar(int16_t iDisplayUpdate)
 {
-  u8g2.setFont(u8g2_font_profont10_mf);
+  g_u8g2.setFont(u8g2_font_profont10_mf);
   char buf[40];
   sprintf(buf, "%ims", iDisplayUpdate);
-  u8g2.drawStr(0, 8, buf);
-  uint8_t percentfull = (millis() / 100) % 100;
-  drawBattery(u8g2.getWidth() - 15, 0, percentfull);
+  g_u8g2.drawStr(0, 8, buf);
+  drawBattery(g_u8g2.getWidth() - 20, 0, g_batteryMonitor.getGauge());
 }
 
 void drawLine(u8g2_uint_t iRow, int16_t iValue, const char *szUnits)
 {
-  u8g2.setFont(u8g2_font_helvR24_tf);
+  g_u8g2.setFont(u8g2_font_helvR24_tf);
   char buf[80];
   sprintf(buf, "%5.3i", iValue);
-  u8g2.drawStr(0, iRow, buf);  
-  u8g2.setFont(u8g2_font_helvR14_tf);
-  u8g2.drawStr(u8g2.getWidth() - 30, iRow, szUnits);
+  g_u8g2.drawStr(0, iRow, buf);  
+  g_u8g2.setFont(u8g2_font_helvR14_tf);
+  g_u8g2.drawStr(g_u8g2.getWidth() - 30, iRow, szUnits);
 }
 
 
 void loop(void) 
 {
+  static unsigned long ulNextUpdate = 0;
   unsigned long ulNow = millis();
+  g_batteryMonitor.updateMaybe(ulNow);
   if(g_button.getAndDispatchKey(ulNow))
   {
-    // return;
-    ;
+    ulNextUpdate = ulNow;
   }
   //
   // freeze display while the key is down
   //
   if(g_button.isKeyDown())
     return;
-  static unsigned long ulNextUpdate = 0;
+  //
+  // is it time to update?
+  //
   if(ulNow < ulNextUpdate)
     return;
   ulNextUpdate = ulNow + (unsigned long)g_iDisplayUpdate;
   //
   // do some measurement
   //
-  float shuntVoltage = ina219.getShuntVoltage_mV();
-  float busVoltage = ina219.getBusVoltage_V();
+  float shuntVoltage = g_ina219.getShuntVoltage_mV();
+  float busVoltage = g_ina219.getBusVoltage_V();
   float loadVoltage_mV = (busVoltage*1000) + shuntVoltage;
-  float current_mA = ina219.getCurrent_mA();
+  float current_mA = g_ina219.getCurrent_mA();
 
   // 
   // update the display
   //
-  u8g2.clearBuffer();                 // clear the internal memory
+  g_u8g2.clearBuffer();                 // clear the internal memory
   
   drawTitleBar(g_iDisplayUpdate);
   
@@ -176,6 +180,6 @@ void loop(void)
   loadVoltage_mV = (float)millis();
   drawLine(iRow, (int16_t)loadVoltage_mV, "mV");
   
-  u8g2.sendBuffer();                  // transfer internal memory to the display
+  g_u8g2.sendBuffer();                  // transfer internal memory to the display
 }
 
